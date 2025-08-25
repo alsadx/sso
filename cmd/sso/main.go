f3b4c194b36d8fcadef4d9c0fbbb2eb6d1b66c39
Alina Sad <alina.sad@icloud.com>
2025-08-25 19:40:45 +0300

    feat: add config

---
+++ cmd/sso/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// TODO: init config object
+
+	// TODO: init logger
+
+	// TODO: init app
+
+	// TODO: run app's gRPC service
+}
---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env        string        `yaml:"env" env-default:"local"`
+	TokenTTL   time.Duration `yaml:"token_ttl" env-required:"true"`
+	Storage    `yaml:"postgres" env-required:"true"`
+	GRPCServer `yaml:"http_server"`
+}
+
+type Storage struct {
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	User     string `yaml:"user" env:"POSTGRES_USER" env-required:"true"`
+	Password string `yaml:"password" env:"POSTGRES_PASSWORD" env-required:"true"`
+	Database string `yaml:"database" env:"POSTGRES_DB" env-required:"true"`
+	MaxConn  int32  `yaml:"max_conn" default:"10"`
+}
+
+type GRPCServer struct {
+	Port    int `yaml:"port"`
+	Timeout int `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	// Загружаем .env
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: no .env file found: %v", err)
+	}
+
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Cannot read config: %s", err)
+	}
+
+	return &cfg
+}
